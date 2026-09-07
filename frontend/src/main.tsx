536ac852f4ca13fc3d667f02ebe243d5070ee511
Pablo Marchina <pablomarchina@gmail.com>
2026-09-06 22:11:48 -0300

    feat(ux): load progressive depth styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AuthBoundary } from "./auth/AuthBoundary";
 import "./styles.css";
+import "./depthTabs.css";
 import "./experience.css";
 import "./explorer.css";
 import "./operations.css";
